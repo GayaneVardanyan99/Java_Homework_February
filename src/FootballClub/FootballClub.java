51a088150871fdd057c398e992e82712fd2ea156
GayaneVardanyan <gayane.vardanyan.99@list.ru>
2023-02-28 12:11:45 +0400

    homework

---
+++ src/FootballClub/FootballClub.java
@@ -0,0 +1,116 @@
+package FootballClub;
+
+public class FootballClub {
+    private String nameOfPlayer;
+    private String surnameOfPlayer;
+    private String playingPositionOfPlayer;
+    private String nationalityOfPlayer;
+
+    FootballClub(String nameOfPlayer, String surnameOfPlayer, String playingPositionOfPlayer, String nationalityOfPlayer) {
+        this.nameOfPlayer = nameOfPlayer;
+        this.surnameOfPlayer = surnameOfPlayer;
+        this.playingPositionOfPlayer = playingPositionOfPlayer;
+        this.nationalityOfPlayer = nationalityOfPlayer;
+    }
+    FootballClub player1 = new FootballClub("Henri", "Avagyan", "goalkeeper", "armenian" );
+    FootballClub player2 = new FootballClub("Aleksandr", "Milkovich", "defender", "serbian");
+    FootballClub player31 = new FootballClub("Gor", "Manukyan", "goalkeeper", "armenian");
+    FootballClub player55 = new FootballClub("Ogney", "Chancharevich", "goalkeeper", "serbian");
+    FootballClub player3 = new FootballClub("Taron", "Voskanyan", "defender", "armenian");
+    FootballClub player4 = new FootballClub("Mladen", "Zelkovich", "defender", "serbian");
+    FootballClub player5 = new FootballClub("Nikita", "Baranov", "defender", "estonian");
+    FootballClub player8 = new FootballClub("Gagik", "Dakhbashyan", "defender", "armenian");
+    FootballClub player13 = new FootballClub("Denis", "Tumasyan", "defender", "armenian");
+    FootballClub player22 = new FootballClub("Hrayr", "Mkoyan", "defender", "armenian");
+    FootballClub player9 = new FootballClub("Artak", "Dashyan", "midfielder", "armenian");
+    FootballClub player11 = new FootballClub("Ghukas", "Poghosyan", "midfielder", "armenian");
+    FootballClub player18 = new FootballClub("Danilo", "Sekulich", "midfielder", "serbian");
+    FootballClub player14 = new FootballClub("Artyom", "Simonyan", "midfielder", "armenian");
+    FootballClub player17 = new FootballClub("Artak", "Edigaryan", "midfielder", "armenian");
+    FootballClub player20 = new FootballClub("Artur", "Edigaryan", "midfielder", "armenian");
+    FootballClub player21 = new FootballClub("Artak", "Grigoryan", "midfielder", "armenian");
+    FootballClub player10 = new FootballClub("Edgar", "Manucharyan", "stricker", "armenian");
+    FootballClub player15 = new FootballClub("Urosh", "Nenandovich", "sticker", "serbian");
+    FootballClub player19 = new FootballClub("Sesar", "Romero", "Sticker", "American");
+
+    public FootballClub getPlayer1() {
+        return player1;
+    }
+
+    public FootballClub getPlayer2() {
+        return player2;
+    }
+
+    public FootballClub getPlayer31() {
+        return player31;
+    }
+
+    public FootballClub getPlayer55() {
+        return player55;
+    }
+
+    public FootballClub getPlayer3() {
+        return player3;
+    }
+
+    public FootballClub getPlayer4() {
+        return player4;
+    }
+
+    public FootballClub getPlayer5() {
+        return player5;
+    }
+
+    public FootballClub getPlayer8() {
+        return player8;
+    }
+
+    public FootballClub getPlayer13() {
+        return player13;
+    }
+
+    public FootballClub getPlayer22() {
+        return player22;
+    }
+
+    public FootballClub getPlayer9() {
+        return player9;
+    }
+
+    public FootballClub getPlayer11() {
+        return player11;
+    }
+
+    public FootballClub getPlayer18() {
+        return player18;
+    }
+
+    public FootballClub getPlayer14() {
+        return player14;
+    }
+
+    public FootballClub getPlayer17() {
+        return player17;
+    }
+
+    public FootballClub getPlayer20() {
+        return player20;
+    }
+
+    public FootballClub getPlayer21() {
+        return player21;
+    }
+
+    public FootballClub getPlayer10() {
+        return player10;
+    }
+
+    public FootballClub getPlayer15() {
+        return player15;
+    }
+
+    public FootballClub getPlayer19() {
+        return player19;
+    }
+}
+
